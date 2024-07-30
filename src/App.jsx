250b240fc36cf1acebc82935e4a99c08c284cc87
BrandonW52 <157122603+BrandonW52@users.noreply.github.com>
2024-07-30 12:42:06 +0200

    Father help

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
+import React from "react";
+import Header from "../components/header";
 
-function App() {
-
+export default function App() {
   return (
-    <>
-     
-    </>
-  )
+    <div>
+      <Header />
+    </div>
+  );
 }
-
-export default App
